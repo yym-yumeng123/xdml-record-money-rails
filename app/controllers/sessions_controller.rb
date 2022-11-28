b97edcd8e48000716207c0043e60298ca1f2b1d8
yym-yumeng123 <18026493873@163.com>
2022-11-28 23:37:29 +0800

    注销 session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  
+    session[:current_user_id] = nil
+    head :ok
   end
 
   def create_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/me", to: "users#me"
+  delete "/sessions", to: "sessions#destroy"
 
   resources :users
 
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:create]
 end
