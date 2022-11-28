8528d926d1807663b453c2c675e0003307d9fbb5
yym-yumeng123 <18026493873@163.com>
2022-11-28 17:57:19 +0800

    limit opmitization code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  # 所有方法都可以 render
+  def render_resource resource
+    if resource.errors.empty?
+      render json: {resouces: resource}, status: 200
+    else
+      render json: {errors: resource.errors}, status: 400
+    end
+  end
 end
