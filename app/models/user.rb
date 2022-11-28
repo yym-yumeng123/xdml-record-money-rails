f0bd709fbbd485120dd6f1b54d93b61932786c71
yym-yumeng123 <18026493873@163.com>
2022-11-28 17:44:33 +0800

    user validation i18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   # validates :password, presence: true, on: :create
   validates :password_confirmation, presence: true, on: :create
-  validates_format_of :email, with: /.+@.+/, on: :create, message: "不符合邮箱规格"
+  validates_format_of :email, with: /.+@.+/, on: :create, if: :email
   # validates :name, length: { minimum: 2 }
   # validates :bio, length: { maximum: 500 }
-  validates :password, length: { in: 6..20 }, on: :create
+  validates :password, length: { in: 6..20 }, on: :create, if: :password
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# 可用的
+I18n.available_locales = [:en, 'zh-CN']
+
+# Set default locale to something other than :en
+I18n.default_locale = 'zh-CN'
