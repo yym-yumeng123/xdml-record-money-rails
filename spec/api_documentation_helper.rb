4be5fb6c5bbe6e1dfc4c5ab0d4028cc6deb9975c
yym-yumeng123 <18026493873@163.com>
2022-11-29 11:13:32 +0800

    api document 完善

---
+++ spec/api_documentation_helper.rb
@@ -0,0 +1,12 @@
+# 修改文档的 Headers
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+RSpec.configure do |config|
+  config.before(:each) do |spec|
+    if spec.metadata[:type].equal? :acceptance
+      header 'Accept', 'application/json'
+      header 'Content-Type', 'application/json'
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,22 @@
+require 'rspec_api_documentation'
+require 'api_documentation_helper'
+
+RspecApiDocumentation.configure do |config|
+  # 请求时我希望展示的
+  config.request_headers_to_include = %w(Content-Type Cookie)
+  # 响应时我希望展示的
+  config.response_headers_to_include = %w(Content-Type Set-Cookie)
+  # body 默认格式
+  config.request_body_formatter = :json
+  # 文档名字
+  config.api_name = "旺财API文档"
+  # 注释
+  config.api_explanation = %q{
+    请求数据格式：默认使用JSON格式，但也支持 application/x-www-form-urlencoded 格式。
+    状态码：200表示成功，400表示失败，422表示表单数据校验不通过，其他状态码请查阅维基百科。
+  }
+end
+
 
 # 写一个通用
 module SpecTestHelper
