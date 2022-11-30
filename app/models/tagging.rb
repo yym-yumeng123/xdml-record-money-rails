9ad923e73f8fcd0889b2517d08b3aed0e30a7194
yym-yumeng123 <18026493873@163.com>
2022-11-30 11:41:02 +0800

    test model

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,5 +1,5 @@
 class Tagging < ApplicationRecord
-  belongs_to :record, class_name: "record", foreign_key: "record_id"
-  belongs_to :tag, class_name: "tag", foreign_key: "tag_id"
+  belongs_to :record
+  belongs_to :tag
   validates :record_id, :tag_id, presence: true
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Tagging, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'create' do
+    # before :each do
+    #   @user = User.create! email: 'agjgsr@qq.com', password: '123456', password_confirmation: '123456'
+    # end
+    it 'require record' do
+      tag = Tag.create! name: 'test'
+      tagging = Tagging.create tag: tag
+      expect(tagging.errors.details[:record][0][:error]).to eq(:blank)
+      expect(tagging.errors[:record][0]).to eq('记录不能为空')
+    end
+    it 'require tag' do
+      # , user: @user
+      record = Record.create! amount: 10000, category: 'outgoings'
+      tagging = Tagging.create record: record
+      expect(tagging.errors.details[:tag][0][:error]).to eq(:blank)
+      expect(tagging.errors[:tag][0]).to eq('标签不能为空')
+    end
+
+    it 'should has record and tag' do
+
+      tag = Tag.create! name: 'test'
+      # , user: @user
+      record = Record.create! amount: 10000, category: 'outgoings'
+      tagging = Tagging.create record: record, tag: tag
+      expect(tag.records.first.id).to eq record.id
+      expect(record.tags.first.id).to eq tag.id
+    end
+
+    it 'should has records and tags' do
+      tag1 = Tag.create! name: 'test'
+      tag2 = Tag.create! name: 'test1'
+      # , user: @user
+      record1 = Record.create! amount: 10000, category: 'outgoings'
+      # , user: @user
+      record2 = Record.create! amount: 10000, category: 'outgoings'
+      Tagging.create record: record1, tag: tag1
+      Tagging.create record: record2, tag: tag1
+      Tagging.create record: record1, tag: tag2
+      Tagging.create record: record2, tag: tag2
+      expect(tag1.records.count).to eq 2
+      expect(tag2.records.count).to eq 2
+      expect(record1.tags.count).to eq 2
+      expect(record2.tags.count).to eq 2
+
+    end
+  end
 end
