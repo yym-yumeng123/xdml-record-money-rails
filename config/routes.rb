a356eabbf9e60893e1943cf5313564233d42c3b6
yym-yumeng123 <18026493873@163.com>
2022-11-30 10:18:52 +0800

    scaffold tag

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/me", to: "users#me"
   delete "/sessions", to: "sessions#destroy"
-
+  
   resources :users
   resources :sessions, only: [:create]
   resources :records
+  resources :tags
 end
